d67df26a3cb973d9a2993b2c0196a65a3c88a53a
Stuti <166793609+stutiinnovates@users.noreply.github.com>
2025-01-26 02:21:15 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,20 @@
+function skillsMember() {
+  return (
+    <div>
+      <div className="member">
+        <div className="member__name">
+          <h2>Jhon Doe</h2>
+        </div>
+        <div className="member__skills">
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JavaScript</li>
+            <li>React</li>
+            <li>Node.js</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
+}
